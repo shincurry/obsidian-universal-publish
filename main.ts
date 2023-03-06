641cc4c5d6c85562672297dbd2b5f50925b2523a
ShinCurry <shincurryyang@gmail.com>
2023-03-06 23:13:50 +0800

    obsidian-universal-publish

--- main.ts
+++ main.ts
@@ -1,78 +1,86 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FileSystemAdapter, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import AdmZip from 'adm-zip';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	serverUrl: string;
+	includeConfigDir: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	serverUrl: '',
+	includeConfigDir: false,
 }
 
-export default class MyPlugin extends Plugin {
+export default class UniPublisherPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		this.addRibbonIcon('send', 'Publish', async (evt: MouseEvent) => {
+			if (!this.settings.serverUrl) {
+				new UniPublisherNotice('Publish server url not set.');
+				return;
+			}
+
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			const notice = new UniPublisherNotice('Collecting content...', 60_000);
+			const vaultPath = (() => {
+				const adapter = app.vault.adapter;
+				if (adapter instanceof FileSystemAdapter) {
+						return adapter.getBasePath();
 				}
+				return null;
+			})();
+			if (!vaultPath) {
+				notice.hide();
+				new UniPublisherNotice('Vault not found.');
+				return;
+			}
+
+			const zip = new AdmZip();
+			await zipDirectoryInsideRecursive(zip, vaultPath, (filename) => {
+				if (filename.endsWith('.DS_Store')) return false;
+				if (!this.settings.includeConfigDir && filename.startsWith('.obsidian')) {
+					return false;
+				}
+				return true;
+			});
+
+			notice.setMessage('Publishing content...')
+			try {
+				const serverUrl = new URL("/publish", this.settings.serverUrl)
+				const data = new FormData()
+				data.append('file', new File([zip.toBuffer()], "content.zip", {
+					type: "application/zip"
+				}))
+				const response = await fetch(serverUrl, {
+					method: "POST",
+					body: data,
+					mode: 'cors',
+				})
+				if (response.status === 204) {
+					notice.hide();
+					new UniPublisherNotice('Published!');
+				} else {
+					notice.hide();
+					new UniPublisherNotice('Publish failed!');
+				}
+			} catch (error) {
+				notice.hide();
+				new UniPublisherNotice('Publish failed!');
 			}
 		});
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new UniPublisherSettingTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -91,26 +99,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class UniPublisherSettingTab extends PluginSettingTab {
+	plugin: UniPublisherPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: UniPublisherPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +112,57 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Universal Publish.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Server URL')
+			.setDesc('The server url of publishing api.')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.serverUrl)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.serverUrl = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Include Config Directory')
+			.setDesc('Whether to include Obsidian Config Directory (.obsidian) When sending content to server.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.includeConfigDir)
+				.onChange(async (value) => {
+					this.plugin.settings.includeConfigDir = value;
 					await this.plugin.saveSettings();
 				}));
 	}
+}
+
+class UniPublisherNotice extends Notice {
+	constructor(message: string | DocumentFragment, timeout?: number) {
+		super(`Universal Publish: ${message}`, timeout)
+	}
+
+	setMessage(message: string | DocumentFragment): this {
+		return super.setMessage(`Universal Publish: ${message}`)
+	}
+}
+
+async function zipDirectoryInsideRecursive(zip: AdmZip, directory: string, filter?: (filename: string) => boolean) {
+	const helper = async (filename: string) => {
+		const lstat = util.promisify(fs.lstat);
+		const readdir = util.promisify(fs.readdir);
+		const zipFile = path.relative(directory, filename);
+		const zipDir = path.dirname(path.relative(directory, filename))
+		if (filter && !filter(zipFile)) return;
+		if ((await lstat(filename)).isDirectory()) {
+			const files = await readdir(filename);
+			for (const file of files) {
+				const filepath = path.join(filename, file);
+				await helper(filepath);
+			}
+		} else {
+			zip.addLocalFile(filename, zipDir === '.' ? undefined : zipDir);
+		}
+	}
+	await helper(directory);
 }
